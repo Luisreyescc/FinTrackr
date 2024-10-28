f9dac0aeef90554da34f3d4acc355446744b31bf
Ianluck Rop <ianluck@ciencias.unam.mx>
2024-10-27 20:31:29 -0600

    Basic and Simple Log-in interface, read the README.md before anything

---
+++ gui/src/router/index.js
@@ -0,0 +1,20 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+  const routes =  [
+    {
+    path: "/",
+    component: () => import('../components/LoginView.vue'),
+  },
+  {
+    path: "/home",
+    component: () => import('../components/WelcomeView.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: routes,
+  linkActiveClass: 'active'
+})
+
+export default router;
