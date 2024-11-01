75afc00b1c10c3bf9e8aec278f3b472342be5a9e
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-11-01 17:13:34 -0600

    Added database models for Users, Incomes, Expenses, Debts, and DebtCategories with corresponding relationships.

---
+++ app/core/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password_hash = models.CharField(max_length=64)
+
+
+class Incomes(models.Model):
+    income_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    source = models.CharField(max_length=255, null=True, blank=True)
+
+
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Expenses(models.Model):
+    expense_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField()
+
+
+class ExpenseCategories(models.Model):
+    expense = models.ForeignKey(Expenses, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+class Debts(models.Model):
+    debt_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    debtor_name = models.CharField(max_length=255, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    is_payed = models.BooleanField(default=False)
+
+
+class DebtCategories(models.Model):
+    debt = models.ForeignKey(Debts, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
