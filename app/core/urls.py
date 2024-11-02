4a487a38a9d9ab171706802c72a8c2ceca1661a1
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-11-02 17:35:46 -0600

    Add new serializers; update corresponding views and URLs.

---
+++ app/core/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path("users/", views.UsersListView.as_view(), name="users-list"),
+    path("users/<int:pk>/", views.UsersDetailView.as_view(), name="users-detail"),
+    path("incomes/", views.IncomesListView.as_view(), name="incomes-list"),
+    path("incomes/<int:pk>/", views.IncomesDetailView.as_view(), name="incomes-detail"),
+    path("categories/", views.CategoriesListView.as_view(), name="categories-list"),
+    path(
+        "categories/<int:pk>/",
+        views.CategoriesDetailView.as_view(),
+        name="categories-detail",
+    ),
+    path("expenses/", views.ExpensesListView.as_view(), name="expenses-list"),
+    path(
+        "expenses/<int:pk>/", views.ExpensesDetailView.as_view(), name="expenses-detail"
+    ),
+    path(
+        "expense-categories/",
+        views.ExpenseCategoriesListView.as_view(),
+        name="expensecategories-list",
+    ),
+    path(
+        "expense-categories/<int:pk>/",
+        views.ExpenseCategoriesDetailView.as_view(),
+        name="expensecategories-detail",
+    ),
+    path("debts/", views.DebtsListView.as_view(), name="debts-list"),
+    path("debts/<int:pk>/", views.DebtsDetailView.as_view(), name="debts-detail"),
+    path(
+        "debt-categories/",
+        views.DebtCategoriesListView.as_view(),
+        name="debtcategories-list",
+    ),
+    path(
+        "debt-categories/<int:pk>/",
+        views.DebtCategoriesDetailView.as_view(),
+        name="debtcategories-detail",
+    ),
+]
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+from rest_framework import generics
+from core.models import (
+    Users,
+    Incomes,
+    Categories,
+    Expenses,
+    ExpenseCategories,
+    Debts,
+    DebtCategories,
+)
+from core.serializers import (
+    UsersSerializer,
+    IncomesSerializer,
+    CategoriesSerializer,
+    ExpensesSerializer,
+    ExpenseCategoriesSerializer,
+    DebtsSerializer,
+    DebtCategoriesSerializer,
+)
 
-# Create your views here.
+
+class UsersListView(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+
+class UsersDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+
+class IncomesListView(generics.ListCreateAPIView):
+    queryset = Incomes.objects.all()
+    serializer_class = IncomesSerializer
+
+
+class IncomesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Incomes.objects.all()
+    serializer_class = IncomesSerializer
+
+
+class CategoriesListView(generics.ListCreateAPIView):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+class CategoriesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+class ExpensesListView(generics.ListCreateAPIView):
+    queryset = Expenses.objects.all()
+    serializer_class = ExpensesSerializer
+
+
+class ExpensesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Expenses.objects.all()
+    serializer_class = ExpensesSerializer
+
+
+class ExpenseCategoriesListView(generics.ListCreateAPIView):
+    queryset = ExpenseCategories.objects.all()
+    serializer_class = ExpenseCategoriesSerializer
+
+
+class ExpenseCategoriesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ExpenseCategories.objects.all()
+    serializer_class = ExpenseCategoriesSerializer
+
+
+class DebtsListView(generics.ListCreateAPIView):
+    queryset = Debts.objects.all()
+    serializer_class = DebtsSerializer
+
+
+class DebtsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Debts.objects.all()
+    serializer_class = DebtsSerializer
+
+
+class DebtCategoriesListView(generics.ListCreateAPIView):
+    queryset = DebtCategories.objects.all()
+    serializer_class = DebtCategoriesSerializer
+
+
+class DebtCategoriesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DebtCategories.objects.all()
+    serializer_class = DebtCategoriesSerializer
