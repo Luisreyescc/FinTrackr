2d6ccc9ed4e0b2e9022e2f55d05603f0b88b2044
Ianluck Rop <ianluck@ciencias.unam.mx>
2024-11-10 13:53:05 -0600

    Email and username validation

--- gui/src/formats/recpassword-form.vue
+++ gui/src/formats/recpassword-form.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="recovery-form">
+<div class="recovery-form">
+  <h2 class="form-title">Recovery Password</h2>
     <label for="username">Username</label>
     <div class="username-container">
+      
       <span class="user-icon gg-user"></span>
       <input
 	v-model="username"
@@ -9,9 +11,10 @@
 	id="username"
 	placeholder="Insert username..."
 	:class="{ 'input-error': usernameError, 'padded-input': true }"
-	@input="clearError('username')" />
+	@input="validateUser" />
     </div>
     <span v-if="usernameError" class="error-message">{{ usernameError }}</span>
+    
     <label for="email">E-mail</label>
     <div class="email-container">
       <span class="email-icon gg-mail"></span>
@@ -21,7 +24,7 @@
 	id="email"
 	placeholder="Insert email..."
 	:class="{ 'input-error': emailError, 'padded-input': true }"
-	@input="clearError('email')" />
+	@input="validateEmail" />
     </div>
     <span v-if="emailError" class="error-message">{{ emailError }}</span>
     
@@ -46,7 +49,9 @@
 </template>
 
 <script>
+import isEmail from "validator/lib/isEmail";
 import '@/css/key.css';
+
 export default {
   name: 'RecoveryForm',
   data() {
@@ -66,8 +71,11 @@ export default {
       this.emailError = '';     
       if (!this.username)
 	this.usernameError = 'Username is required';
+      else if (this.username.length < 3) this.usernameError = 'Username requieres at least 3 characters';
       if (!this.email)
 	this.emailError = 'Email is required';
+      else if (!isEmail(this.email))
+	this.emailError = 'Invalid email format';
       if (this.username && this.email) {
         this.codeSent = true;
         alert('A recovery code has been sent to your email!');
@@ -84,8 +92,19 @@ export default {
 	this.keyError = 'Please insert the code sent to your e-mail';
       }
     },
+    validateUsername() {
+      this.usernameError = '';
+      if (this.username.length > 0 && this.username.length < 3) {
+        this.usernameError = 'Username must be at least 3 characters long';
+      }
+    },
+    validateEmail() {
+      this.emailError = '';
+      if (this.email && !isEmail(this.email)) {
+        this.emailError = 'Invalid email format';
+      }
+    },
     clearError(field) {
-      // Clear specific error messages when the user starts typing
       if (field === 'username')
 	this.usernameError = '';
       if (field === 'email')
--- gui/src/formats/signup-form.vue
+++ gui/src/formats/signup-form.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="signup-form">
   <h2 class="form-title">Sign Up</h2>
+  
   <label for="username">Username</label>
   <div class="username-container">
     <span class="user-icon gg-user"></span>
@@ -10,20 +11,22 @@
       id="username"
       placeholder="Enter a username..."
       :class="{ 'input-error': usernameError, 'padded-input': true }"
-      @input="clearError('username')"/>
+      @input="validateUser"/>
   </div>
   <span v-if="usernameError" class="error-message">{{ usernameError }}</span>
-  <label for="email">E-mail</label>
-  <div class="email-container">
-    <span class="email-icon gg-mail"></span>
-    <input
-      v-model="email"
-      type="email"
-      id="email"
-      placeholder="Enter your email..."
-      :class="{ 'input-error': emailError, 'padded-input': true }"
-      @input="clearError('email')"/>
-  </div>
+
+    <label for="email">E-mail</label>
+    <div class="email-container">
+      <span class="email-icon gg-mail"></span>
+      <input
+        v-model="email"
+        type="email"
+        id="email"
+        placeholder="Enter your email..."
+        :class="{ 'input-error': emailError, 'padded-input': true }"
+        @input="validateEmail"
+      />
+    </div>
   <span v-if="emailError" class="error-message">{{ emailError }}</span>
   
   <label for="password">Password</label>
@@ -72,6 +75,7 @@
 </template>
 
 <script>
+import isEmail from "validator/lib/isEmail";
 import '@/css/mail.css';
 
 export default {
@@ -98,17 +102,33 @@ export default {
       this.confirmPasswordError = '';
       if (!this.username)
 	this.usernameError = 'Username is required';
+      else if (this.username.length < 3) this.usernameError = 'Username requieres at least 3 characters';
       if (!this.email)
 	this.emailError = 'Email is required';
+      else if (!isEmail(this.email))
+	this.emailError = 'Invalid email format';
       if (!this.password)
 	this.passwordError = 'Password is required';
       if (!this.conf_password)
 	this.confirmPasswordError = 'Confirm password is required';
       if (this.password && this.conf_password && this.password !== this.conf_password) {
+	this.passwordError = 'Passwords don\'t match';
 	this.confirmPasswordError = 'Passwords don\'t match';
       }
       if (!this.usernameError && !this.emailError && !this.passwordError && !this.confirmPasswordError) {
 	this.$emit('signUp', { username: this.username, email: this.email, password: this.password});
+      }
+    },
+    validateUsername() {
+      this.usernameError = '';
+      if (this.username.length > 0 && this.username.length < 3) {
+        this.usernameError = 'Username must be at least 3 characters long';
+      }
+    },
+    validateEmail() {
+      this.emailError = '';
+      if (this.email && !isEmail(this.email)) {
+        this.emailError = 'Invalid email format';
       }
     },
     clearError(field) {
