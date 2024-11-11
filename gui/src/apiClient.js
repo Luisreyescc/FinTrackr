3d2253ece34344570a7817683d9ae91d548cd8c4
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-11-10 22:33:57 -0600

    Updated README.md and apiClient.js

--- gui/src/apiClient.js
+++ gui/src/apiClient.js
@@ -7,12 +7,23 @@ const apiClient = axios.create({
   },
 });
 
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (
+      token &&
+      !config.url.includes("register/") &&
+      !config.url.includes("login/")
+    ) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default apiClient;
