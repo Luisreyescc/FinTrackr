6dd3973a15378b751216f2a87c00785916591de9
Ianluck Rop <ianluck@ciencias.unam.mx>
2024-11-15 21:26:49 -0600

    Moved incomes files from components to components/incomes/ directory

--- gui/src/formats/home-form.vue
+++ gui/src/formats/home-form.vue
@@ -51,9 +51,9 @@
 
 <script>
 import axios from 'axios';
-import IncomesForm from '@/components/incomes-forms.vue';
-import IncomeButton from '@/components/incomes-header.vue';
-import IncomeRow from '@/components/income-row.vue';
+import IncomesForm from '@/components/incomes/incomes-forms.vue';
+import IncomeButton from '@/components/incomes/incomes-header.vue';
+import IncomeRow from '@/components/incomes/income-row.vue';
 
 import ExpensesForm from '@/components/expenses/expenses-forms.vue';
 import ExpenseButton from '@/components/expenses/expenses-header.vue';
