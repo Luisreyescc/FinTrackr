029ff2fadbc76bcb3f2e14585c688189744f4731
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-12-01 17:21:19 -0600

    Fixed some problems with income. The PUT is now handled correctly.

--- gui/src/components/incomes/incomes-forms.vue
+++ gui/src/components/incomes/incomes-forms.vue
@@ -1,95 +1,141 @@
 <template>
-<div class="form-container" >
-  <h3 class="form-title">New Income Data</h3>
-  <div class="form-content scrollbar">
-    <form @submit.prevent="submitForm">
-      <label>
-	Amount:
-	<input
-          type="text"
-          v-model="income.amount"
-          @input="validateAmount"
-          :class="{ 'input-error': amountError, 'input-valid': !amountError && income.amount }"
-          placeholder="Enter amount (e.g., 1000.00)" />
-      </label>
-      <span v-if="amountError" class="error-message">{{ amountError }}</span>
-      
-      <div class="categories-wrapper">
-	<div class="categories-select" @click="toggleDropdown">
-          Sources
-          <span class="dropdown-icon">
-            <font-awesome-icon v-if="!dropdownOpen" :icon="['fas', 'angle-right']" />
-            <font-awesome-icon v-else :icon="['fas', 'angle-down']" />
-          </span>
-	</div>
-	
-	<ul v-if="dropdownOpen" class="categories-dropdown scrollbar">
-          <li v-if="loadingCategories">Loading categories...</li>
-          <li v-else @click="showNewCategoriesDialog" style="color: green; font-weight: bold;">
-            <font-awesome-icon :icon="['fas', 'plus']" font-size="12" /> New source
-          </li>
-          <li
-            v-for="(category, index) in ategoriesOptions"
-            :key="index"
-            @click="newCategory(category)">{{ category }}
-          </li>
-	</ul>
-	
-	<div v-if="showNewCategory" class="overlay" @click="cancelNewCategory"></div>
-	<div v-if="showNewCategory" class="new-category-dialog">
-          <h4>Enter new source</h4>
+  <div class="form-container">
+    <h3 class="form-title">New Income Data</h3>
+    <div class="form-content scrollbar">
+      <form @submit.prevent="submitForm">
+        <label>
+          Amount:
           <input
             type="text"
-            v-model="newCategory"
-            placeholder="New source"
-            :maxlength="18" />
-          <div class="button-group">
-            <button @click="cancelNewCategory" class="cancel-category">Cancel</button>
-            <button
-              @click="acceptNewCategory"
-              class="accept-category"
-              :disabled="!isAcceptEnabled">Accept</button>
+            v-model="income.amount"
+            @input="validateAmount"
+            :class="{
+              'input-error': amountError,
+              'input-valid': !amountError && income.amount,
+            }"
+            placeholder="Enter amount (e.g., 1000.00)"
+          />
+        </label>
+        <span v-if="amountError" class="error-message">{{ amountError }}</span>
+
+        <div class="categories-wrapper">
+          <div class="categories-select" @click="toggleDropdown">
+            Sources
+            <span class="dropdown-icon">
+              <font-awesome-icon
+                v-if="!dropdownOpen"
+                :icon="['fas', 'angle-right']"
+              />
+              <font-awesome-icon v-else :icon="['fas', 'angle-down']" />
+            </span>
           </div>
-	</div>
-	
-	<div class="selected-categories">
-          <span v-for="(category, index) in income.categories" :key="index" class="tag">
-            {{ category }}
-            <button @click="removeCategory(index)" class="close-button">
-              <font-awesome-icon :icon="['fas', 'xmark']"/>
-            </button>
-          </span>
-	</div>
-      </div>
-      
-      <label>
-	Description:
-	<input
-          type="text"
-          v-model="income.description"
-          @input="validateTextField('description')"
-          :class="{ 'input-error': descriptionError, 'input-valid': !descriptionError && income.description }"
-          placeholder="Enter a description for the income" />
-      </label>
-      <span v-if="descriptionError" class="error-message">{{ descriptionError }}</span>
-      
-      <label>
-	Date:
-	<input
-          type="date"
-          v-model="income.date"
-          @input="validateDate"
-          :class="{ 'input-error': dateError, 'input-valid': !dateError && income.date }" />
-      </label>
-      <span v-if="dateError" class="error-message">{{ dateError }}</span>
-      
-      <div class="button-group">
-	<button type="button" @click="cancelForm" class="cancel-button">Cancel</button>
-	<button type="submit" class="submit-button" :disabled="!isSubmitEnabled">Submit</button>
-      </div>
-    </form>
+
+          <ul v-if="dropdownOpen" class="categories-dropdown scrollbar">
+            <li v-if="loadingCategories">Loading categories...</li>
+            <li
+              v-else
+              @click="showNewCategoryDialog"
+              style="color: green; font-weight: bold"
+            >
+              <font-awesome-icon :icon="['fas', 'plus']" font-size="12" /> New
+              source
+            </li>
+            <li
+              v-for="(category, index) in categoriesOptions"
+              :key="index"
+              @click="newCategory(category)"
+            >
+              {{ category }}
+            </li>
+          </ul>
+
+          <div
+            v-if="showNewCategory"
+            class="overlay"
+            @click="cancelNewCategory"
+          ></div>
+          <div v-if="showNewCategory" class="new-category-dialog">
+            <h4>Enter new source</h4>
+            <input
+              type="text"
+              v-model="newCategory"
+              placeholder="New source"
+              :maxlength="18"
+            />
+            <div class="button-group">
+              <button @click="cancelNewCategory" class="cancel-category">
+                Cancel
+              </button>
+              <button
+                @click="acceptNewCategory"
+                class="accept-category"
+                :disabled="!isAcceptEnabled"
+              >
+                Accept
+              </button>
+            </div>
+          </div>
+
+          <div class="selected-categories">
+            <span
+              v-for="(category, index) in income.categories"
+              :key="index"
+              class="tag"
+            >
+              {{ category }}
+              <button @click="removeCategory(index)" class="close-button">
+                <font-awesome-icon :icon="['fas', 'xmark']" />
+              </button>
+            </span>
+          </div>
+        </div>
+
+        <label>
+          Description:
+          <input
+            type="text"
+            v-model="income.description"
+            @input="validateTextField('description')"
+            :class="{
+              'input-error': descriptionError,
+              'input-valid': !descriptionError && income.description,
+            }"
+            placeholder="Enter a description for the income"
+          />
+        </label>
+        <span v-if="descriptionError" class="error-message">{{
+          descriptionError
+        }}</span>
+
+        <label>
+          Date:
+          <input
+            type="date"
+            v-model="income.date"
+            @input="validateDate"
+            :class="{
+              'input-error': dateError,
+              'input-valid': !dateError && income.date,
+            }"
+          />
+        </label>
+        <span v-if="dateError" class="error-message">{{ dateError }}</span>
+
+        <div class="button-group">
+          <button type="button" @click="cancelForm" class="cancel-button">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            class="submit-button"
+            :disabled="!isSubmitEnabled"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -140,9 +186,12 @@ export default {
         }
 
         this.loadingCategories = true;
-        const response = await axios.get('http://localhost:8000/api/income-categories/', {
-          headers: { Authorization: `Bearer ${token}` },
-        });
+        const response = await axios.get(
+          "http://localhost:8000/api/income-categories/",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          },
+        );
         this.categoryOptions = response.data.categories;
       } catch (error) {
         console.error("Error fetching categories:", error);
@@ -152,7 +201,10 @@ export default {
     },
     async sendNewCategory() {
       try {
-        const response = await axios.post('http://localhost:8000/api/categories/', { name: this.newCategory.trim() });
+        const response = await axios.post(
+          "http://localhost:8000/api/categories/",
+          { name: this.newCategory.trim() },
+        );
         if (response.status === 201) {
           // Add new category to the user's categories table if the backend respons with success
           this.categoryOptions.push(this.newCategory.trim());
@@ -164,9 +216,9 @@ export default {
         console.error("Error adding new category:", error);
       }
     },
-    addCategory(categories) {
-      if (!this.income.categories.includes(categories)) {
-        this.income.categories.push(categories);
+    addCategory(category) {
+      if (!this.income.categories.includes(category)) {
+        this.income.categories.push(category);
       }
       this.dropdownOpen = false;
     },
@@ -180,9 +232,10 @@ export default {
     async acceptNewCategory() {
       if (this.newCategory.trim()) {
         await this.sendNewCategory();
-        this.income.categories.push(this.newCategory);
-        this.showNewCategory = false;
+        this.categoryOptions.push(this.newCategory.trim());
+        this.income.categories.push(this.newCategory.trim());
         this.newCategory = "";
+        this.showNewCategory = false;
         this.dropdownOpen = false;
       }
     },
@@ -254,285 +307,285 @@ export default {
 
 <style scoped>
 .form-container {
-    padding: 20px;
-    border-radius: 8px;
-    font-family: "Wix Madefor Display", sans-serif;
-    box-sizing: border-box;
+  padding: 20px;
+  border-radius: 8px;
+  font-family: "Wix Madefor Display", sans-serif;
+  box-sizing: border-box;
 }
 
 .form-title {
-    font-size: 22px;
-    font-weight: bold;
-    color: #333;
-    text-align: left;
-    margin-bottom: 40px;
-    color: #21255b;
-    font-family: "Wix Madefor Display", sans-serif;
+  font-size: 22px;
+  font-weight: bold;
+  color: #333;
+  text-align: left;
+  margin-bottom: 40px;
+  color: #21255b;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .form-content {
-    max-height: calc(80vh - 220px);
-    padding: 10px;
-    margin: 0;
-    overflow-y: auto;
-    scrollbar-width: thin;
-    scrollbar-color: #1b1f9c #e0e0e0;
+  max-height: calc(80vh - 220px);
+  padding: 10px;
+  margin: 0;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: #1b1f9c #e0e0e0;
 }
 
 label {
-    display: block;
-    margin-bottom: 10px;
-    font-weight: bold;
-    font-size: 18px;
-    color: #333;
-    text-align: left;
-    font-family: "Wix Madefor Display", sans-serif;
+  display: block;
+  margin-bottom: 10px;
+  font-weight: bold;
+  font-size: 18px;
+  color: #333;
+  text-align: left;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 input {
-    width: 90%;
-    padding: 14px;
-    margin-top: 10px;
-    margin-bottom: 2px;
-    border: none;
-    outline: none;
-    background-color: #f0f0f0;
-    border-radius: 4px 4px 0 0;
-    border-bottom: 2px solid #ccc;
-    transition:
-	background-color 0.3s,
-	border-color 0.3s;
-    font-family: "Wix Madefor Display", sans-serif;
+  width: 90%;
+  padding: 14px;
+  margin-top: 10px;
+  margin-bottom: 2px;
+  border: none;
+  outline: none;
+  background-color: #f0f0f0;
+  border-radius: 4px 4px 0 0;
+  border-bottom: 2px solid #ccc;
+  transition:
+    background-color 0.3s,
+    border-color 0.3s;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .input-error {
-    border-bottom-color: #e42121;
-    background-color: #ffebee;
-    outline: none;
+  border-bottom-color: #e42121;
+  background-color: #ffebee;
+  outline: none;
 }
 
 .input-valid {
-    border-bottom-color: #1b1f9c;
-    background-color: #e0f7fa;
-    outline: none;
+  border-bottom-color: #1b1f9c;
+  background-color: #e0f7fa;
+  outline: none;
 }
 
 .error-message {
-    color: #e42121;
-    font-size: 12px;
-    margin-top: -10px;
-    margin-bottom: 10px;
-    text-align: left;
+  color: #e42121;
+  font-size: 12px;
+  margin-top: -10px;
+  margin-bottom: 10px;
+  text-align: left;
 }
 
 .button-group {
-    display: flex;
-    gap: 10px;
-    margin-top: 80px;
-    justify-content: space-between;
+  display: flex;
+  gap: 10px;
+  margin-top: 80px;
+  justify-content: space-between;
 }
 
 .cancel-button {
-    background-color: #333;
-    color: white;
-    border: none;
-    padding: 13px 30px;
-    border-radius: 2px;
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: bold;
-    font-family: "Wix Madefor Display", sans-serif;
+  background-color: #333;
+  color: white;
+  border: none;
+  padding: 13px 30px;
+  border-radius: 2px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .submit-button {
-    background-color: #4caf50;
-    color: white;
-    border: none;
-    padding: 13px 30px;
-    border-radius: 3px;
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: bold;
-    font-family: "Wix Madefor Display", sans-serif;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  padding: 13px 30px;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .cancel-button:hover {
-    background-color: #616161;
+  background-color: #616161;
 }
 
 .submit-button:hover {
-    background-color: #237242;
+  background-color: #237242;
 }
 
 .categories-wrapper {
-    position: relative;
-    margin-bottom: 20px;
-    font-family: "Wix Madefor Display", sans-serif;
+  position: relative;
+  margin-bottom: 20px;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .categories-select {
-    padding: 12px 15px;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    width: 85px;
-    font-weight: bold;
-    background-color: #ffffff;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.2s;
+  padding: 12px 15px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  width: 85px;
+  font-weight: bold;
+  background-color: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.2s;
 }
 
 .categories-select:hover {
-    background-color: #f8f9fa;
+  background-color: #f8f9fa;
 }
 
 .dropdown-icon {
-    width: 16px;
-    height: 16px;
-    margin-left: 8px;
-    transform: translateY(-1px);
-    color: #21255b;
+  width: 16px;
+  height: 16px;
+  margin-left: 8px;
+  transform: translateY(-1px);
+  color: #21255b;
 }
 
 .categories-dropdown {
-    position: absolute;
-    top: 80%;
-    left: 0;
-    right: 0;
-    border: 1px solid #ddd;
-    border-radius: 12px;
-    background-color: white;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    padding: 0;
-    list-style: none;
-    z-index: 1000;
-    overflow-y: auto;
-    max-height: 160px;
-    max-width: 200px;
-    animation: fadeIn 0.2s ease-out;
+  position: absolute;
+  top: 80%;
+  left: 0;
+  right: 0;
+  border: 1px solid #ddd;
+  border-radius: 12px;
+  background-color: white;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 0;
+  list-style: none;
+  z-index: 1000;
+  overflow-y: auto;
+  max-height: 160px;
+  max-width: 200px;
+  animation: fadeIn 0.2s ease-out;
 }
 
 .categories-dropdown li {
-    padding: 10px 20px;
-    cursor: pointer;
-    transition:
-	background-color 0.3s,
-	color 0.3s;
-    text-align: left;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition:
+    background-color 0.3s,
+    color 0.3s;
+  text-align: left;
 }
 
 .categories-dropdown li:hover {
-    background-color: #f0f8ff;
-    border-radius: 12px;
-    color: #1010ac;
-    font-weight: bold;
+  background-color: #f0f8ff;
+  border-radius: 12px;
+  color: #1010ac;
+  font-weight: bold;
 }
 
 .selected-categories {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 10px;
-    gap: 5px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  gap: 5px;
 }
 
 .tag {
-    display: flex;
-    align-items: center;
-    border-radius: 16px;
-    padding: 5px 10px;
-    font-weight: bold;
-    background-color: #f3f3f9;
-    border: 1px solid #6f6f7a;
+  display: flex;
+  align-items: center;
+  border-radius: 16px;
+  padding: 5px 10px;
+  font-weight: bold;
+  background-color: #f3f3f9;
+  border: 1px solid #6f6f7a;
 }
 
 .close-button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    margin-right: -10px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  margin-right: -10px;
 }
 
 .close-button .gg-close {
-    font-size: 12px;
-    color: #333;
+  font-size: 12px;
+  color: #333;
 }
 
 .new-categories-dialog {
-    position: fixed;
-    background: white;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    padding: 15px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    z-index: 1100;
-    width: 220px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  position: fixed;
+  background: white;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1100;
+  width: 220px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .new-categories-dialog h4 {
-    margin: 0 0 10px;
-    font-size: 18px;
-    color: #21255b;
-    font-family: "Wix Madefor Display", sans-serif;
+  margin: 0 0 10px;
+  font-size: 18px;
+  color: #21255b;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .new-categories-dialog input {
-    width: 90%;
-    padding: 14px;
-    margin-top: 10px;
-    border: none;
-    outline: none;
-    background-color: #f0f0f0;
-    border-radius: 4px 4px 0 0;
-    border-bottom: 2px solid #ccc;
-    transition:
-	background-color 0.3s,
-	border-color 0.3s;
-    font-family: "Wix Madefor Display", sans-serif;
+  width: 90%;
+  padding: 14px;
+  margin-top: 10px;
+  border: none;
+  outline: none;
+  background-color: #f0f0f0;
+  border-radius: 4px 4px 0 0;
+  border-bottom: 2px solid #ccc;
+  transition:
+    background-color 0.3s,
+    border-color 0.3s;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .new-categories-dialog .button-group {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
 }
 
 .cancel-categories,
 .accept-categories {
-    color: white;
-    border: none;
-    padding: 8px 18px;
-    border-radius: 2px;
-    cursor: pointer;
-    font-size: 14px;
-    font-weight: bold;
-    font-family: "Wix Madefor Display", sans-serif;
+  color: white;
+  border: none;
+  padding: 8px 18px;
+  border-radius: 2px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: "Wix Madefor Display", sans-serif;
 }
 
 .cancel-categories {
-    background-color: #333;
+  background-color: #333;
 }
 
 .accept-categories {
-    background-color: #4caf50;
+  background-color: #4caf50;
 }
 
 .cancel-button:hover {
-    background-color: #616161;
+  background-color: #616161;
 }
 
 .submit-button:hover {
-    background-color: #237242;
+  background-color: #237242;
 }
 
 .submit-button:disabled,
 .accept-categories:disabled {
-    background-color: #a2cbb2;
-    cursor: not-allowed;
-    opacity: 0.7;
+  background-color: #a2cbb2;
+  cursor: not-allowed;
+  opacity: 0.7;
 }
 </style>
