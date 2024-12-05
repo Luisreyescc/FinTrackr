4929791eceb3c249686db6eaa6f594a3e858b0a7
Ianluck Rop <ianluck@ciencias.unam.mx>
2024-12-05 01:20:58 -0600

    New Users page and new nav-item 'Users' for admin to manage users in database

---
+++ gui/src/pages/Users.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="users-page">
+  <h1>Users Management</h1>
+  <p>Only admins can view this page.</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UsersManagment",
+};
+</script>
+
+<style scoped>
+.users-page {
+  padding: 20px;
+}
+</style>
